4b3329f9652dfa10c994784735df6a325c574bd5
Eric Zhang <superezzhang@gmail.com>
2024-12-03 21:56:59 -0500

    Add button listeners for rest of buttons

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const numberButtons = document.querySelectorAll('button[data-type="number"]');
 const operationButtons = document.querySelectorAll('button[data-type="operation"]');
+const specialButtons = document.querySelectorAll('button[data-type="special"]');
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function(){
@@ -10,6 +11,12 @@ numberButtons.forEach(button => {
 operationButtons.forEach(button => {
     button.addEventListener('click', function(){
         console.log('Operation is: ' + this.dataset.value);
+    });
+});
+
+specialButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        console.log('Special button is: ' + this.dataset.value);
     });
 });
 
